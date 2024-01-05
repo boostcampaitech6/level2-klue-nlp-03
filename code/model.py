3c06fca827348e80ddcb0243ac95d1a3559480d4
dustnehowl <dustnrkfnfn@gmail.com>
2024-01-05 13:51:15 +0000

    feat : modulize model

---
+++ code/model.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+from transformers import AutoModel, AutoModelForSequenceClassification, AutoConfig
+
+
+# model for Trainer
+class Model(nn.Module):
+    def __init__(self, model_name, num_labels):
+        super(Model, self).__init__()
+        self.config = AutoConfig.from_pretrained(model_name)
+        self.config.num_labels = num_labels
+        self.plm = AutoModelForSequenceClassification.from_pretrained(model_name, config=self.config)
+        self.loss = nn.CrossEntropyLoss()
+        self.save_pretrained = self.plm.save_pretrained
+
+    def training_step(self, batch):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        token_type_ids = batch['token_type_ids']
+        labels = batch['labels']
+
+        outputs = self.plm(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, labels=labels)
+        loss = outputs.loss
+        return loss
+
+    def forward(self, input_ids, attention_mask, token_type_ids, labels=None):
+        outputs = self.plm(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        if labels is not None:
+            loss = self.loss(outputs.logits, labels)
+            return {'loss': loss, 'logits': outputs.logits }
+        return {'logits': outputs.logits }
+
+
+        
--- code/train.py
+++ code/train.py
@@ -8,6 +8,8 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_sc
 from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
 from load_data import *
+from model import Model
+from random import *
 
 
 def klue_re_micro_f1(preds, labels):
@@ -42,14 +44,13 @@ def klue_re_auprc(probs, labels):
 
 def compute_metrics(pred):
   """ validation을 위한 metrics function """
-  labels = pred.label_ids
-  preds = pred.predictions.argmax(-1)
-  probs = pred.predictions
+  logits, labels = pred
+  preds = logits.argmax(-1)
+  probs = logits
 
-  # calculate accuracy using sklearn's function
   f1 = klue_re_micro_f1(preds, labels)
   auprc = klue_re_auprc(probs, labels)
-  acc = accuracy_score(labels, preds) # 리더보드 평가에는 포함되지 않습니다.
+  acc = accuracy_score(labels, preds)
 
   return {
       'micro f1 score': f1,
@@ -67,76 +68,51 @@ def label_to_num(label):
   return num_label
 
 def train():
-  # load model and tokenizer
-  # MODEL_NAME = "bert-base-uncased"
   MODEL_NAME = "team-lucid/deberta-v3-base-korean"
   tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-  # load dataset
   train_dataset = load_data("../../dataset/train/train.csv")
-  # dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
-
   train_label = label_to_num(train_dataset['label'].values)
-  # dev_label = label_to_num(dev_dataset['label'].values)
 
-  # train test stlit
   train_X, dev_X, train_Y, dev_Y = train_test_split(train_dataset, train_label, test_size=0.1)
 
-  # tokenizing dataset
   tokenized_train = tokenized_dataset(train_X, tokenizer)
   tokenized_dev = tokenized_dataset(dev_X, tokenizer)
-  # ori
-  # tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-  # tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
 
-  # make dataset for pytorch.
   RE_train_dataset = RE_Dataset(tokenized_train, train_Y)
   RE_dev_dataset = RE_Dataset(tokenized_dev, dev_Y)
 
   device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-  print(device)
-  # setting model hyperparameter
-  model_config =  AutoConfig.from_pretrained(MODEL_NAME)
-  model_config.num_labels = 30
-
-  model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
-  print(model.config)
-  model.parameters
+  model = Model(MODEL_NAME, num_labels=30)
   model.to(device)
   
-  # 사용한 option 외에도 다양한 option들이 있습니다.
-  # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
   training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    save_total_limit=5,              # number of total save model.
-    save_steps=500,                 # model saving step.
-    num_train_epochs=10,              # total number of training epochs
-    learning_rate=5e-5,               # learning_rate
-    per_device_train_batch_size=16,  # batch size per device during training
-    per_device_eval_batch_size=16,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-    logging_steps=100,              # log saving step.
-    evaluation_strategy='epoch', # evaluation strategy to adopt during training
-                                # `no`: No evaluation during training.
-                                # `steps`: Evaluate every `eval_steps`.
-                                # `epoch`: Evaluate every end of epoch.
-    eval_steps = 500,            # evaluation step.
-    load_best_model_at_end = True 
+    output_dir='./results',
+    save_total_limit=1,
+    save_steps=1500,
+    num_train_epochs=1,
+    learning_rate=5e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs',
+    logging_steps= 1500,
+    evaluation_strategy='steps',
+    eval_steps = 1500,
+    load_best_model_at_end = True,
   )
   trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=RE_train_dataset,         # training dataset
-    eval_dataset=RE_dev_dataset,             # evaluation dataset
-    compute_metrics=compute_metrics         # define metrics function
+    model=model,
+    args=training_args,
+    train_dataset=RE_train_dataset,
+    eval_dataset=RE_dev_dataset,
+    compute_metrics=compute_metrics
   )
 
-  # train model
   trainer.train()
   model.save_pretrained('./best_model')
+
 def main():
   train()
 
