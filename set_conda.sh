494751f3975d4df7445ad20d3289a7866c8c4511
jingi-data <imfeelo@gmail.com>
2024-01-12 12:57:38 +0900

    chore: updated 'environment.yaml'

--- set_conda.sh
+++ set_conda.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # If conda has already installed
 conda config --add channels conda-forge
-conda update -c defaults conda
+conda update -n base -c defaults conda
 conda install -c conda-forge mamba
 mamba env create -f environment.yaml
+
+## If env name is 'klue'
+# conda init zsh
+# conda activate klue
